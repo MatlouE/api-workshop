50ad62f3c75f47201261b571270a9bddbd015e97
MatlouE <149234501+MatlouE@users.noreply.github.com>
2026-02-24 11:03:56 +0000

    modified:   api0Auth2Client.py

--- api0Auth2Client.py
+++ api0Auth2Client.py
@@ -9,3 +9,17 @@ class 0Auth2Client:
         self.token_url = token_url
         self.access_token = None 
         self.refresh_token = None
+
+    def get_access_token(self):
+        'retrieve access token usng client credentials grant'
+        data = {
+            'grant_type': 'client_credentials',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret
+        }
+
+        response = requests.post(self.token_url, data=data)
+
+        token_data = response.json()
+        self.access_token = token_data.get('access_token')
+        self.refresh_token = token_data.get('refresh_token')
