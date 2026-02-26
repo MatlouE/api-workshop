a7dc27325e803bf770cc3a5b1b6e14a2681eceeb
MatlouE <149234501+MatlouE@users.noreply.github.com>
2026-02-26 08:14:45 +0000

    modified:   api0Auth2Client.py

--- api0Auth2Client.py
+++ api0Auth2Client.py
@@ -19,7 +19,36 @@ class 0Auth2Client:
         }
 
         response = requests.post(self.token_url, data=data)
+        if response.status_code == 200: 
+            token_data = response.json()
+            self.access_token = token_data.get('access_token')
+            self.refresh_token = token_data.get('refresh_token')
+            return self.access_token 
+        else:
+            raise exception(f'failed to get token: {response.text}')
 
-        token_data = response.json()
-        self.access_token = token_data.get('access_token')
-        self.refresh_token = token_data.get('refresh_token')
+    def make_authenticated_request(self, url, method=G='GET', **kwargs)
+        'Make an authenticated api request'
+        if not self.access_token:
+            self.get_access_token()
+
+        headers = kwargs.pop('headers', {})
+        headers['Authorization'] = f'Bearer {self.access_token}'
+
+        response = requests.request(method, url, headers = headers, **kwargs) 
+
+        #if token is expired, refresh and retry
+        if response.status_code == 401 :
+            self.get_access_token =()
+            headers['Authorization'] = f'bearer {self.access_token}'
+            response = requests.request(method, url, headers=headers, **kwargs)
+
+        return response
+
+0auth_client = 0Auth2Client(
+    client_id ='your_client_id',
+    client_secret = 'your_client_secret',
+    token_url = 'https://auth.example.com/auth/token'
+)
+
+response = 0auth_client.make_authenticated_request()
